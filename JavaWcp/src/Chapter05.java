12ab3f4bd4521c58620643ccb5518a1b21971531
Lispooh13 <riaya1203@gmail.com>
2021-11-17 12:07:25 +0900

    [Finish]Java5章

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,22 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long l = 111222333444555L;
+		System.out.println(l);
+		double d = 1234567.89;
+		System.out.println(d);
+		boolean b = true;
+		System.out.println(b);
+		
+		Float f = 1.123f;
+		System.out.println(f);
+		Integer i = 6;
+		System.out.println(i);
+		Character c = 'B';
+		System.out.println(c);
+		
+		int[] array = { 10, 20, 30, 40, 50 };
+		System.out.println(array[3]);
+
+	}
+}
