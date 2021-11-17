52034ef6e16bde70164d80c6096447011f5d65c4
Lispooh13 <riaya1203@gmail.com>
2021-11-17 11:16:33 +0900

    [Finish]Java4章

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 1;
+		String str = "lisa Suemori";
+		System.out.println(str);		
+		str = "幸運は勇者を好む";
+		System.out.println(str);		
+		System.out.println(LUCKY_NUMBER);
+	}
+}
