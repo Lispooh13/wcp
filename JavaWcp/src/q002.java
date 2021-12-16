0d36f6fa9f8638452443dc098a22f9da9dd3ff39
Lispooh13 <riaya1203@gmail.com>
2021-12-16 19:29:04 +0900

    練習問題

--- JavaWcp/src/q002.java
+++ JavaWcp/src/q002.java
@@ -2,7 +2,7 @@
 public class q002 {
 	public static void main(String[] args) {
         int[] array = { 1, 2, 3, 4, 5 };
-        //ここにプログラムを書いていきます。
+        
         for (int i = 0; i < array.length; i++) {
             array[i] *= 2;
         }
--- JavaWcp/src/q003.java
+++ JavaWcp/src/q003.java
@@ -1,4 +1,13 @@
 
 public class q003 {
+	public static void main(String[] args) {
+		int[] a = { 1, 2, 3, 4, 5 };
+        int[] b = { 6, 7, 8, 9, 10 };
+        int[] c = new int[5];
 
+        for (int i = 0; i < c.length; i++) {
+        	c[i]= a[i] + b[1];
+            System.out.println(c[i]);
+        }
+	}
 }
