ca9f1859c61f3b52377cbbd73921956a07f90350
Lispooh13 <riaya1203@gmail.com>
2021-12-18 23:15:06 +0900

    練習問題

--- JavaWcp/src/q004.java
+++ JavaWcp/src/q004.java
@@ -3,11 +3,11 @@ public class q004 {
 	public static void main(String[] args) {
 	//1~100までの奇数の和
 		int oddSum = 0;
-		for(int i = 1; i <= 100 ; i++) {
-			if(i % 2 != 0) {
+		for(int i = 1; i <= 100; i++) {
+			if (i % 2 != 0) {
 				oddSum += i;
 			}
 		}
-		System.out.println(oddSum);	
+		System.out.println(oddSum);
 	}
 }
--- JavaWcp/src/q007.java
+++ JavaWcp/src/q007.java
@@ -1,14 +1,15 @@
 
-public class q006 {
+public class q007 {
 	public static void main(String[] args) {
-		for (int i = 0; i < 5; i++) {
-			for(int j = 0; j < 5-(i+1); j++){
+		for(int i = 0; i < 5; i++) {
+			for (int j = 0; j < i; j++) {
 				System.out.print(" ");
 			}
-			for(int k = 0; k < (i+1)*2-1; k++) {
+			for (int k = 0; k <= (5-i)*2-1; k++){
 				System.out.print("*");
 			}
 			System.out.println();
 		}
 	}
 }
+
