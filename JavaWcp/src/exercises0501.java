720d7a723ec05b4926b25323993d0404725449f1
Lispooh13 <riaya1203@gmail.com>
2021-12-20 21:49:40 +0900

    練習問題

--- JavaWcp/src/exercises0501.java
+++ JavaWcp/src/exercises0501.java
@@ -1,9 +1,11 @@
 
 public class exercises0501 {
 	public static void main(String[] args) {
-		int[] array = { 1, 2, 3, 4, 5 };
-		for(int n = 0; n<=array.length ; n++) {
-			System.out.println(array[n]);
+		//10個の数値を2倍にして表示
+		int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		for(int n = 0; n < array.length; n++) {
+			int result = array[n]*2;
+			System.out.println(result);
 		}
 	}
 }
---
+++ JavaWcp/src/exercises0502.java
@@ -0,0 +1,10 @@
+
+public class exercises0502 {
+	public static void main(String[] args) {
+		int[] array = {1, 2, 3, 4, 5};
+		for(int n = 4; n < array.length; n--) {
+			int result = array[n];
+			System.out.print(result+ " ");
+		}
+	}
+}
