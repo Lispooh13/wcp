9079c1d8bdb35bd4d7cb18598d88476f5e363e44
Lispooh13 <riaya1203@gmail.com>
2021-12-23 18:24:50 +0900

    練習問題

---
+++ JavaWcp/src/fun604.java
@@ -0,0 +1,12 @@
+
+public class fun604 {
+	public static void main(String[] args) {
+		int Sum = 0;
+		for(int i=1; i<20; i++) {
+			if(i%2 != 0) {
+				Sum += i;
+				System.out.println(Sum);
+			}
+		}
+	}
+}
---
+++ JavaWcp/src/q009.java
@@ -0,0 +1,9 @@
+
+public class q009 {
+	public static void main(String[] args) {
+		int number = 3;
+		for(int i = 1; i <=9; i++) {
+			System.out.println(number* i);
+		}
+	}
+}
