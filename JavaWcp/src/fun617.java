bbf4a17583019911c9150a43dfe40251a74aba5f
Lispooh13 <riaya1203@gmail.com>
2021-12-26 22:39:46 +0900

    練習問題

---
+++ JavaWcp/src/fun617.java
@@ -0,0 +1,9 @@
+
+public class fun617 {
+	public static void main(String[] args) {
+		int[] a = {2, 4, 1, 3, 5, 7, 2, 1, 4, 8};
+		for(int i = 0; i < 10; i++) {
+			System.out.println(a[i]);
+		}
+	}
+}
---
+++ JavaWcp/src/fun618.java
@@ -0,0 +1,12 @@
+
+public class fun618 {
+	public static void main(String[] args) {
+		int[] a = {2, 4, 1, 3, 5, 7, 2, 1, 4, 8};
+		int sum;
+		sum = 0;
+		for(int i = 0; i < 10; i++) {
+			sum += a[i];
+		}
+		System.out.println("合計=" + sum);
+	}
+}
