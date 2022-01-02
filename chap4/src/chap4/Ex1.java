51736f0b951e2b4e2be217e5d3bd02c714dea296
Lispooh13 <riaya1203@gmail.com>
2022-01-02 23:45:13 +0900

    練習問題

---
+++ chap4/src/chap4/Ex1.java
@@ -0,0 +1,13 @@
+package chap4;
+
+public class Ex1 {
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 3;
+		int c = a + b;
+		
+		System.out.println("aは"+ a );
+		System.out.println("bは"+ b );
+		System.out.println("cは"+ c );
+	}
+}
