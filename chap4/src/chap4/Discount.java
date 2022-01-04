02dfac9ba67ac568413e06d75a6dd16d57e1192a
Lispooh13 <riaya1203@gmail.com>
2022-01-04 23:24:09 +0900

    練習問題

---
+++ chap4/src/chap4/Discount.java
@@ -0,0 +1,9 @@
+package chap4;
+
+public class Discount {
+	public static void main(String[] args) {
+		int FirstPrice = 1000;
+		double Discount = FirstPrice * 0.7;
+		System.out.println(Math.floor(Discount));
+	}
+}
