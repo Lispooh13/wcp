5551a574dd1dc135e394c278d5265bc4630e4189
Lispooh13 <riaya1203@gmail.com>
2022-01-05 23:39:08 +0900

    練習問題

--- chap4/src/chap4/Discount.java
+++ chap4/src/chap4/Discount.java
@@ -4,6 +4,7 @@ public class Discount {
 	public static void main(String[] args) {
 		int FirstPrice = 1000;
 		double Discount = FirstPrice * 0.7;
+//		小数点以下切り捨て
 		System.out.println(Math.floor(Discount));
 	}
 }
---
+++ chap4/src/chap4/Reverse.java
@@ -0,0 +1,9 @@
+package chap4;
+
+public class Reverse {
+	public static void main(String[] args) {
+		int a = 58;
+		int b = 85;
+		System.out.println(a +"の反対は"+ b + "です");
+	}
+}
