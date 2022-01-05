7db01cbce26b249cf9341546cff82001fd0446e3
Lispooh13 <riaya1203@gmail.com>
2022-01-06 00:04:30 +0900

    練習問題

---
+++ chap4/src/chap4/TimeToTime.java
@@ -0,0 +1,13 @@
+package chap4;
+
+public class TimeToTime {
+	public static void main(String[] args) {
+		int a = 9;
+		int b = 10;
+		int c = 12;
+		int d = 20;
+		int e = c-a;
+		int f = d-b;
+		System.out.println(a +"時"+ b +"分から"+c+"時"+d+"分までは"+e+"時間"+f+"分です");
+	}
+}
