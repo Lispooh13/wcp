112589cd8b9dff8cf016a2e99ae26bfd0b4fd049
Lispooh13 <riaya1203@gmail.com>
2022-01-07 23:51:00 +0900

    練習問題

---
+++ chap5/src/chap5/Age1.java
@@ -0,0 +1,12 @@
+package chap5;
+
+public class Age1 {
+	public static void main(String[] args) {
+		int age = 15;
+		System.out.println("年齢は"+ age + "歳");
+		if(age >= 18) {
+			System.out.println("どうぞ");
+		}
+			System.out.println("おしまい");
+	}
+}
