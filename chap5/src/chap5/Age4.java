413364e3de3aee8d687dad6fc9358f2792ffdd34
Lispooh13 <riaya1203@gmail.com>
2022-01-16 00:03:03 +0900

    練習問題

--- chap5/src/chap5/Age4.java
+++ chap5/src/chap5/Age4.java
@@ -1,5 +1,13 @@
 package chap5;
 
 public class Age4 {
+	public static void main(String[] args) {
+		int age = 15;
+		if (age >= 18) {
+			System.out.println("はい");
+		}else {
+			System.out.println("お断り");
+		}
 
+	}
 }
--- chap5/src/chap5/ThreeFive1.java
+++ chap5/src/chap5/ThreeFive1.java
@@ -1,5 +1,11 @@
 package chap5;
 
 public class ThreeFive1 {
+	public static void main(String[] args) {
+		int num = 15;
+		if(num % 3 == 0 && num % 5 ==0) {
+			System.out.println("3でも5でも割り切れます。");
 
+	}
+	}
 }
